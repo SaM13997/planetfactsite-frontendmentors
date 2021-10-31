aaae930a5b240078d0ba2705ae37bf2e73efd94d
Sarthak Malhotra <49312722+SaM13997@users.noreply.github.com>
2021-10-31 12:02:34 +0530

    removed the background to check if stutter fixes

---
+++ App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import mySvg from '../src/background-stars.svg'
+import Header from './Header';
+import Planet from './Planet'
+import Info from './Info'
+import { useState, } from 'react';
+
+function App() {
+
+  let [currPlanet, setCurrPlanet] = useState('earth')
+
+
+  let handlePlanetChange = (planet) => {
+    let curpl = planet.toLowerCase()
+    setCurrPlanet(curpl)
+  }
+
+
+
+  return(
+    <div id="base" className='base'>
+    
+      <div className="base-subwrapper">
+  
+        <div className="header">
+          <Header planetHandler={handlePlanetChange} />
+        </div>
+        <div className="planet">
+          <Planet key={currPlanet} planet={currPlanet}/>
+        </div>
+        <div className="info">
+          <Info key={currPlanet} planet={currPlanet} />
+        </div>
+      </div>
+    </div>  
+
+  )
+}
+// TODO: add the hover states for the buttons --DONE
+// TODO: add css to Info comp elements --DONE
+// todo add animations with anime.js
+export default App;
